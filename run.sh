4e024f8870ca964f34173f9920b65ba45ba9743b
Andreas <andreas.giannoutsos@gmail.com>
2021-05-03 00:13:14 +0300

    first commit

---
+++ run.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+make
+java Main Example.java
---
+++ src/ClassVisitor.java
@@ -0,0 +1,16 @@
+package src;
+
+import syntaxtree.*;
+import visitor.*;
+
+
+public class ClassVisitor{
+
+    public ClassVisitor(){
+        System.out.println("naii helooo");
+    }
+
+
+
+    
+}
