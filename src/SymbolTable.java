8c603aa211bf27de8a48a8686048bb256953244c
Andreas <andreas.giannoutsos@gmail.com>
2021-05-06 21:34:19 +0300

    comments

--- src/SymbolTable.java
+++ src/SymbolTable.java
@@ -6,40 +6,14 @@ import java.util.*;
 public class SymbolTable extends TreeMap<String, Symbol>{
     String name;
 
-    // static <K,V extends Comparable<? super V>> SortedSet<Map.Entry<String,V>> getSortedd(SymbolTable<V> map) {
-    //     SortedSet<SymbolTable.Entry<V>> sortedEntries = new TreeSet<SymbolTable.Entry<V>>( new Comparator<SymbolTable.Entry<V>>() {
-    //         @Override
-    //         public int compare(SymbolTable.Entry<V> e1, SymbolTable.Entry<V> e2) {
-    //             int res = e1.getValue().compareTo(e2.getValue());
-    //             return res != 0 ? res : 1;
-    //         }
-    //     });
-    //     sortedEntries.addAll(map.entrySet());
-    //     return sortedEntries;
-    // }
-    
     public SymbolTable(String name){        
-        // super(new Comparator<String>() {
-        //     @Override public int compare(String s1, String s2) {
-        //         return 0;
-        //     }           
-        // } );
         this.name = name;
     }
     public SymbolTable(){        
         this.name = null;
     }
 
-    // public Symbol get(String key){
-    //     System.out.println("oeorerer");
-    //     if (key.startsWith("Class "))
-    //         return super.get(key.replaceFirst("Class ", ""));
-    //     else    
-    //         return super.get(key);
-    // }
-
     // code from here https://stackoverflow.com/questions/2864840/treemap-sort-by-value
-    // erwthsh giati uparxei h java? 
     public SortedSet<Map.Entry<String, Symbol>> getSorted(){
 
         SortedSet<Map.Entry<String, Symbol>> sortedEntries = new TreeSet<Map.Entry<String, Symbol>>( new Comparator<Map.Entry<String, Symbol>>() {
